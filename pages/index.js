a72d96646f0a83615ce788a21aaaa1cd5d1de7d3
Eman-coder00 <eman07pedro@gmail.com>
2025-12-18 14:27:22 +0000

    adiciona `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
-function home(){
-    return <h1>teste 2</h1>
+function home() {
+  return <h1>teste 2</h1>;
 }
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
+}
+
 export default home;
